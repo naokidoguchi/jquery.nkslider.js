55d46ff3ca5f51223ba1c14432ee0df55436ba45
naokidoguchi <naoaru.28@gmail.com>
2016-03-14 18:56:43 +0900

    update

--- src/jquery.nkslider.js
+++ src/jquery.nkslider.js
@@ -2,76 +2,76 @@
 
 	$.fn.nkslider = function(option) {
 
-		var defaults = {size: 300};
-		var option = $.extend(defaults,option);
+		var defaults = {size: 300},
+		option = $.extend(defaults,option),
+		$slider = this;
 
-	$(function() {
-			var $slider = $('.slider');
+		if($slider.attr('class') == 'slider') {
+			$(function() {
 
-			setInterval(slider, 2000);
+				setInterval(slider, 2000);
+				
+				function slider() {
 
-			function slider() {
+					var wrap = $slider.width(),
+					size = - defaults.size,
+					int = parseInt(size),
+					sAbs = Math.abs(int),
+					left = $slider.css('left'),
+					mLeft = parseInt(left),
+					slideSize = Math.abs(mLeft);
 
-				var wrap = $slider.width(),
+					if(slideSize >= wrap - sAbs) {
+						$slider.animate({left: '0'}, 'slow');
+					} else {
+						$slider.animate({left: '+=' + int + 'px'}, 'slow');
+					}
+				}
+
+			});
+		}
+		if($slider.attr('class') == 'slider2') {
+			$(function() {
+
+				var $prev = $('span:first-child'),
+				$next = $('span:nth-child(2)'),
+				$item = $('.slider2 li'),
+
+				wrap = $slider.width(),
 				size = - defaults.size,
 				int = parseInt(size),
 				sAbs = Math.abs(int),
-				left = $slider.css('left'),
-				mLeft = parseInt(left),
-				slideSize = Math.abs(mLeft);
+				left,
+				mLeft,
+				slideSize = Math.abs(mLeft),
+				item = $item.length,
+				clickCount = 1;
 
-				if(slideSize >= wrap - sAbs) {
-					$slider.animate({left: '0'}, 'slow');
-				} else {
-					$slider.animate({left: '+=' + int + 'px'}, 'slow');
-				}
-			}
+				$prev.click(function() {
+					if(clickCount <= 1) {
+						return false;
+					} else {
+						$slider.animate({left: '+=' + sAbs + 'px'}, 'slow');
+						clickCount--;
+					}
 
-		});
+				});
 
-		// slider2
-		$(function() {
+				$next.click(function() {
+					left = $slider.css('left');
+					mLeft = Math.abs(left);
 
-			var $slider = $('.slider--2'),
-			$prev = $('span:first-child'),
-			$next = $('span:nth-child(2)'),
-			$item = $('.slider__item--2'),
+					if(clickCount > item - 1) {
+						return false;
+					} else {
+						$slider.animate({left: '+=' + int + 'px'}, 'slow');
+						clickCount++;
+					}
 
-			wrap = $slider.width(),
-			size = - defaults.size,
-			int = parseInt(size),
-			sAbs = Math.abs(int),
-			left,
-			mLeft,
-			slideSize = Math.abs(mLeft),
-			item = $item.length,
-			clickCount = 1;
-
-			$prev.click(function() {
-				if(clickCount <= 1) {
-					return false;
-				} else {
-					$slider.animate({left: '+=' + sAbs + 'px'}, 'slow');
-					clickCount--;
-				}
+				});
 
 			});
-
-			$next.click(function() {
-				left = $slider.css('left');
-				mLeft = Math.abs(left);
-
-				if(clickCount > item - 1) {
-					return false;
-				} else {
-					$slider.animate({left: '+=' + int + 'px'}, 'slow');
-					clickCount++;
-				}
-
-			});
-
-		});
-
+		}
 		return(this);
 	};
 })(jQuery);
